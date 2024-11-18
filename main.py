dffcc1050c916bc043ae8150e515dfabde42ab91
kylec <krclark8485@hotmail.com>
2024-11-17 16:45:27 -0800

    added additional comments to main.py

--- main.py
+++ main.py
@@ -31,6 +31,9 @@ def calculate_stats(data_set: DataSet):
                       'mode': [],
                       'outliers': []}
     
+    # Encodes DataSet object data_set into JSON encodable object,
+    # then extracts data set input list from new_data_set; then
+    # sorts new_data_set for use in subsequent math_methods
     new_data_set = jsonable_encoder(data_set)
     new_data_set = new_data_set["data_input"]
     new_data_set.sort()
