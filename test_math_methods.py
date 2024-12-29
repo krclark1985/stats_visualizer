cd66b8e533cb12fae5ee4b85c7a30d619bcc1465
kylec <krclark8485@hotmail.com>
2024-12-28 19:58:36 -0800

    cleaned up test_math_methods.py, updated comments

--- test_math_methods.py
+++ test_math_methods.py
@@ -1,9 +1,5 @@
 import math_methods, pytest
 
-# would be violating DRY principle to repeatedly check for
-# empty data set or non-numeric data set; this check should
-# be done in main.py (and then tested there, too!)
-
 # these data sets represent various iterations of numerical
 # data sets that might be received as input
 zeroes_data_set = [0, 0]
@@ -13,8 +9,6 @@ negative_data_set = [-3, -2, -1]
 larger_data_set = [-8, -8, -2, -1, 1, 1, 8, 8, 9, 10]
 mult_values_data_set = [0.2, 0.2, 0.22, 0.22, 0.22, 3, 3, 3]
 outliers_data_set = [5, 98, 101, 101, 101, 102, 103, 103, 396]
-# non_numeric_data_set = ['a', 'b', 'c']
-# empty_data_set = []
 
 @pytest.mark.parametrize("input_data_set, expected_mean", 
                          [(zeroes_data_set, 0),
@@ -24,8 +18,6 @@ outliers_data_set = [5, 98, 101, 101, 101, 102, 103, 103, 396]
                           (larger_data_set, 1.8),
                           (mult_values_data_set, 1.2575),
                           (outliers_data_set, 1110 / 9)])
-                          # (non_numeric_data_set, "data set contains non-numeric values; please input numeric values only"),
-                          # (empty_data_set, "data set contains no values; please input numeric values")])
 def test_mean(input_data_set, expected_mean):
     assert math_methods.mean(input_data_set) == expected_mean
 
